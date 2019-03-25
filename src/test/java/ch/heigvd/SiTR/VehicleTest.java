161e5095746c3a832c9ef505744d75e650b924f3
Simon Walther <simon.walt@hotmail.com>
2019-03-25 09:55:48 +0100

    fixes #17

--- src/test/java/ch/heigvd/SiTR/VehicleTest.java
+++ src/test/java/ch/heigvd/SiTR/VehicleTest.java
@@ -1,43 +1,52 @@
 package ch.heigvd.SiTR;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
 
 /**
  * Unit test for Vehicle.
  */
-public class VehicleTest extends TestCase {
+public class VehicleTest {
 
-    public void testPosition() {
+    @Test
+    public void position() {
         Vehicle vehicle = new Vehicle(1.6, 145.2);
         vehicle.setPosition(10.5);
         assertEquals(vehicle.getPosition(), 10.5);
     }
 
-    public void testSpeed() {
+    @Test
+    public void speed() {
         Vehicle vehicle = new Vehicle(1.6, 145.2);
         vehicle.setSpeed(63.5);
         assertEquals(vehicle.getSpeed(), 63.5);
     }
 
-    public void testLength() {
+    @Test
+    public void length() {
         Vehicle vehicle = new Vehicle(1.6, 145.2);
         assertEquals(vehicle.getLength(), 1.6);
     }
 
-    public void testMaxSpeed() {
+    @Test
+    public void maxSpeed() {
         Vehicle vehicle = new Vehicle(1.6, 145.2);
         assertEquals(vehicle.getMaxSpeed(), 145.2);
     }
 
+    @Test
     public void speedShouldNotExceedMaxSpeed() {
         Vehicle vehicle = new Vehicle(1.6, 145.2);
         vehicle.setSpeed(250);
         assertEquals(vehicle.getSpeed(), 145.2);
     }
 
-    public void testFrontVehicle() {
+    @Test
+    public void frontVehicle() {
         Vehicle vehicle = new Vehicle(1.6, 145.2);
         Vehicle vehicle2 = new Vehicle(1.6, 145.2);
 
@@ -46,7 +55,8 @@ public class VehicleTest extends TestCase {
         assertNull(vehicle2.getFrontVehicle());
     }
 
-    public void testFrontDistance() {
+    @Test
+    public void frontDistance() {
         Vehicle vehicle = new Vehicle(1.6, 145.2);
         vehicle.setPosition(50);
         Vehicle vehicle2 = new Vehicle(1.7, 145.2);
@@ -55,6 +65,7 @@ public class VehicleTest extends TestCase {
         assertEquals(vehicle.frontDistance(), 68.35);
     }
 
+    @Test
     public void frontDistanceShouldBeInfiniteIfThereIsntFrontVehicle() {
         Vehicle vehicle = new Vehicle(1.6, 145.2);
         assertEquals(vehicle.frontDistance(), Double.POSITIVE_INFINITY);
