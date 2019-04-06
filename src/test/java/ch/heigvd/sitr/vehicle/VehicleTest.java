0b250ba65c49886dc7820e1884c8ea4526662409
Simon Walther <simon.walt@hotmail.com>
2019-04-06 11:08:14 +0200

    refactor vehicle tests with beforeEach

--- src/test/java/ch/heigvd/sitr/vehicle/VehicleTest.java
+++ src/test/java/ch/heigvd/sitr/vehicle/VehicleTest.java
@@ -5,6 +5,7 @@
 
 package ch.heigvd.sitr.vehicle;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,95 +16,96 @@ import static org.junit.jupiter.api.Assertions.assertNull;
  * @author Simon Walther
  */
 public class VehicleTest {
+    Vehicle vehicle = null;
+    Vehicle frontVehicle = null;
+    VehicleController vehicleController = null;
+
+    @BeforeEach
+    public void createDummyVehicleController() {
+        vehicleController = new VehicleController();
+        vehicleController.setDesiredVelocity(33.33);
+        vehicleController.setMinimumSpacing(2);
+        vehicleController.setDesiredTimeHeadway(1.5);
+        vehicleController.setMaxAcceleration(0.3);
+        vehicleController.setComfortableBrakingDeceleration(3);
+    }
+
+    @BeforeEach
+    public void createDummyVehicle() {
+        frontVehicle = new Vehicle(vehicleController, 1.7, 33.33);
+        vehicle = new Vehicle(vehicleController, 1.6, 33.33);
+        vehicle.setFrontVehicle(frontVehicle);
+    }
 
     @Test
     public void position() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
         vehicle.setPosition(10.5);
         assertEquals(10.5, vehicle.getPosition());
     }
 
     @Test
     public void speed() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
         vehicle.setSpeed(25.5);
         assertEquals(25.5, vehicle.getSpeed());
     }
 
     @Test
     public void length() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
         assertEquals(1.6, vehicle.getLength());
     }
 
     @Test
     public void maxSpeed() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
-        assertEquals(44.44, vehicle.getMaxSpeed());
+        assertEquals(33.33, vehicle.getMaxSpeed());
     }
 
     @Test
     public void speedShouldNotExceedMaxSpeed() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
         vehicle.setSpeed(50);
-        assertEquals(44.44, vehicle.getSpeed());
+        assertEquals(33.33, vehicle.getSpeed());
     }
 
     @Test
     public void negativeSpeedShouldNotExceedMaxSpeed() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
         vehicle.setSpeed(-50);
-        assertEquals(-44.44, vehicle.getSpeed());
+        assertEquals(-33.33, vehicle.getSpeed());
     }
 
     @Test
     public void frontVehicle() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
-        Vehicle vehicle2 = new Vehicle(null, 1.6, 44.44);
-
-        vehicle.setFrontVehicle(vehicle2);
-        assertEquals(vehicle2, vehicle.getFrontVehicle());
-        assertNull(vehicle2.getFrontVehicle());
+        vehicle.setFrontVehicle(frontVehicle);
+        assertEquals(frontVehicle, vehicle.getFrontVehicle());
+        assertNull(frontVehicle.getFrontVehicle());
     }
 
     @Test
     public void frontDistance() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
         vehicle.setPosition(50);
-        Vehicle vehicle2 = new Vehicle(null, 1.7, 44.44);
-        vehicle2.setPosition(120);
-        vehicle.setFrontVehicle(vehicle2);
+        frontVehicle.setPosition(120);
         assertEquals(68.35, vehicle.frontDistance());
     }
 
     @Test
     public void frontDistanceShouldBeInfiniteIfThereIsntFrontVehicle() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
-        assertEquals(Double.POSITIVE_INFINITY, vehicle.frontDistance());
+        assertEquals(Double.POSITIVE_INFINITY, frontVehicle.frontDistance());
     }
 
     @Test
     public void vehicleController() {
-        VehicleController vehicleController = new VehicleController();
-        Vehicle vehicle = new Vehicle(vehicleController, 1.6, 44.44);
         assertEquals(vehicleController, vehicle.getVehicleController());
     }
 
     @Test
     public void relSpeed() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
         vehicle.setSpeed(12.5);
-        Vehicle frontVehicle = new Vehicle(null, 1.6, 44.44);
         frontVehicle.setSpeed(32);
-        vehicle.setFrontVehicle(frontVehicle);
         assertEquals(-19.5, vehicle.relSpeed());
     }
 
     @Test
     public void relSpeedWithNoFrontVehicle() {
-        Vehicle vehicle = new Vehicle(null, 1.6, 44.44);
-        vehicle.setSpeed(22.22);
-        assertEquals(0.0, vehicle.relSpeed());
+        frontVehicle.setSpeed(22.22);
+        assertEquals(0.0, frontVehicle.relSpeed());
     }
 
     /**
@@ -136,26 +138,16 @@ public class VehicleTest {
      */
     @Test
     public void updateSpeed() {
-        // define controller
-        VehicleController vehicleController = new VehicleController();
-        vehicleController.setDesiredVelocity(33.33);
-        vehicleController.setMinimumSpacing(2);
-        vehicleController.setDesiredTimeHeadway(1.5);
-        vehicleController.setMaxAcceleration(0.3);
-        vehicleController.setComfortableBrakingDeceleration(3);
-
-        Vehicle frontVehicle = new Vehicle(vehicleController, 1.6, 33.33);
         frontVehicle.setSpeed(27.77);
         frontVehicle.setPosition(100);
 
-        Vehicle vehicle = new Vehicle(vehicleController, 1.6, 33.33);
         vehicle.setSpeed(22.22);
         vehicle.setPosition(80);
         vehicle.setFrontVehicle(frontVehicle);
 
         vehicle.updateSpeed(10);
 
-        assertEquals(24.59196317300287, vehicle.getSpeed());
+        assertEquals(24.592, vehicle.getSpeed(), 0.001);
     }
 
     /**
@@ -174,23 +166,12 @@ public class VehicleTest {
      */
     @Test
     public void updateSpeedExceedingMaxSpeed() {
-        // define controller
-        VehicleController vehicleController = new VehicleController();
-        vehicleController.setDesiredVelocity(33.33);
-        vehicleController.setMinimumSpacing(2);
-        vehicleController.setDesiredTimeHeadway(1.5);
-        vehicleController.setMaxAcceleration(0.3);
-        vehicleController.setComfortableBrakingDeceleration(3);
-
-        Vehicle frontVehicle = new Vehicle(vehicleController, 1.6, 33.33);
         frontVehicle.setSpeed(27.77);
         frontVehicle.setPosition(100);
 
-        Vehicle vehicle = new Vehicle(vehicleController, 1.6, 33.33);
         vehicle.setSpeed(22.22);
         vehicle.setPosition(80);
         vehicle.setFrontVehicle(frontVehicle);
-
         vehicle.updateSpeed(60);
 
         assertEquals(33.33, vehicle.getSpeed());
@@ -227,27 +208,16 @@ public class VehicleTest {
      */
     @Test
     public void updatePosition() {
-        // define controller
-        VehicleController vehicleController = new VehicleController();
-        vehicleController.setDesiredVelocity(33.33);
-        vehicleController.setMinimumSpacing(2);
-        vehicleController.setDesiredTimeHeadway(1.5);
-        vehicleController.setMaxAcceleration(0.3);
-        vehicleController.setComfortableBrakingDeceleration(3);
-
-        Vehicle frontVehicle = new Vehicle(vehicleController, 1.6, 33.33);
         frontVehicle.setSpeed(27.77);
         frontVehicle.setPosition(100);
 
-        Vehicle vehicle = new Vehicle(vehicleController, 1.6, 33.33);
         vehicle.setSpeed(22.22);
         vehicle.setPosition(80);
         vehicle.setFrontVehicle(frontVehicle);
         vehicle.setPosition(50);
-
         vehicle.updatePosition(10);
 
-        assertEquals(296.22284806492166, vehicle.getPosition());
+        assertEquals(296.223, vehicle.getPosition(), 0.001);
 
     }
 }
