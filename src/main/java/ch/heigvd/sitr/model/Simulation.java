003e6e526553c6b15d506eb1341a40e330c671cd
Simon Walther <simon.walt@hotmail.com>
2019-04-08 19:39:31 +0200

    add scale to simulation #76

--- src/main/java/ch/heigvd/sitr/model/Simulation.java
+++ src/main/java/ch/heigvd/sitr/model/Simulation.java
@@ -5,12 +5,24 @@
 
 package ch.heigvd.sitr.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Simulation class handles all global simulation settings and values
  *
- * @author Luc Wachter
+ * @author Luc Wachter, Simon Walther
  */
 public class Simulation {
+    // the scale of m/px
+    @Getter
+    @Setter
+    public static double scale;
+
+    public Simulation(double scale) {
+        this.scale = scale;
+    }
+
     public static double mpsToKph(double mps) {
         // m/s => km/h : x * 3.6
         return mps * 3.6;
@@ -20,4 +32,6 @@ public class Simulation {
         // km/h => m/s : x / 3.6
         return kph / 3.6;
     }
+
+
 }
