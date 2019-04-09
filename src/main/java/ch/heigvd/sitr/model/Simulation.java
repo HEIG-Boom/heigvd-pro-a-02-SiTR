a6a1eede6994110baa218841e9bf56053b58d065
Laykel <layke.luc@gmail.com>
2019-04-09 23:38:47 +0200

    Added proper comments in Simulation

--- src/main/java/ch/heigvd/sitr/model/Simulation.java
+++ src/main/java/ch/heigvd/sitr/model/Simulation.java
@@ -11,11 +11,23 @@ package ch.heigvd.sitr.model;
  * @author Luc Wachter
  */
 public class Simulation {
+    /**
+     * Convert meters per second to kilometers per hour
+     *
+     * @param mps The amount of m/s to convert
+     * @return the corresponding amount of km/h
+     */
     public static double mpsToKph(double mps) {
         // m/s => km/h : x * 3.6
         return mps * 3.6;
     }
 
+    /**
+     * Convert kilometers per hour to meters per second
+     *
+     * @param kph The amount of km/h to convert
+     * @return the corresponding
+     */
     public static double kphToMps(double kph) {
         // km/h => m/s : x / 3.6
         return kph / 3.6;
