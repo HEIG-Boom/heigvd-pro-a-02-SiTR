7854d204088a28e0086ee87fd97a90a5ab478e62
Mateo Tutic <mateo@tutic.ch>
2019-04-10 17:25:36 +0200

    Add physical lane in LaneSegment with a specific constructor

--- src/main/java/ch/heigvd/sitr/map/LaneSegment.java
+++ src/main/java/ch/heigvd/sitr/map/LaneSegment.java
@@ -3,8 +3,10 @@ package ch.heigvd.sitr.map;
 import ch.heigvd.sitr.vehicle.Vehicle;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 public class LaneSegment implements Iterable<Vehicle> {
 
@@ -29,76 +31,85 @@ public class LaneSegment implements Iterable<Vehicle> {
     private int xCoordinatesEnd;
     @Getter
     private int yCoordinatesEnd;
+    @Getter
+    private final int lane; // physical lane
 
-    private LinkedList<Vehicle> listVehicle = new LinkedList<>();
+    private List<Vehicle> vehicles = new ArrayList<>();
 
     LaneSegment(RoadSegment roadSegment, int xCoordinatesBegin, int yCoordinatesBegin, int xCoordinatesEnd, int yCoordinatesEnd,
-                int length, float width, int speedLimit, int laneSegmentId) {
+                int length, float width, int speedLimit, int laneSegmentId, int lane) {
         this.roadSegment = roadSegment;
         this.length = length;
         this.width = width;
         this.laneSegmentId = laneSegmentId;
         this.speedLimit = speedLimit;
-        listVehicle = new LinkedList<>();
+        this.lane = lane;
+        vehicles = new LinkedList<>();
         this.xCoordinatesBegin = xCoordinatesBegin;
         this.yCoordinatesBegin = yCoordinatesBegin;
         this.xCoordinatesEnd = xCoordinatesEnd;
         this.yCoordinatesEnd = yCoordinatesEnd;
     }
 
-    public boolean checkIfLaneOnLeft() {
-        return roadSegment.checkIfLaneOnLeft(laneSegmentId);
-    }
-
-    public LaneSegment getLaneOnLeft(int laneId) {
-        return roadSegment.getLaneOnLeft(laneId);
-    }
-
-    public LaneSegment getLaneOnRight(int laneId) {
-        return roadSegment.getLaneOnRight(laneId);
-    }
-
-    /*Ne fonctionne que pour une ligne droite*/
-    public boolean checkIfChangeLaneIsSafe(double position, LaneSegment lane) {
-        if (lane.laneSegmentId != laneSegmentId) {
-            return roadSegment.checkIfChangeLaneIsSafe(position, lane);
-        }
-        Vehicle vehicle = listVehicle.getFirst();
-        for (int i = 1; position > vehicle.getPosition() && i < listVehicle.size(); i++) {
-            if ((position - vehicle.getPosition()) < 5) {
-                return false;
-            }
-            vehicle = listVehicle.get(i);
-        }
-        if ((vehicle.getPosition() - position) < 5) {
-            return false;
-        }
-        return true;
-    }
-
-    /*Ajoute un véhicule en les classant par leur position sur la voie
-     * le premier véhicule dans la liste est le plus proche du début de la ligne, le dernier le plus éloigné
-     * !! Ne contrôle pas si le véhicule est déjà dans cette voie*/
-    public void addVehicle(Vehicle vehicle) {
-        int i;
-        for (i = 0; i < listVehicle.size(); i++) {
-            if (vehicle.getPosition() > listVehicle.get(i).getPosition()) {
-                i++;
-                break;
-            }
-        }
-        i--;
-        listVehicle.add(i, vehicle);
-        nbVehicle++;
-    }
-
-    public void removeVehicle(Vehicle vehicle) {
-        listVehicle.remove(vehicle);
-        nbVehicle --;
+    LaneSegment(RoadSegment roadSegment, int lane) {
+        this.roadSegment = roadSegment;
+        this.lane = lane;
     }
 
     @Override
     public Iterator<Vehicle> iterator() {
-        return listVehicle.iterator();
+        return vehicles.iterator();
     }
+
+//    public boolean checkIfLaneOnLeft() {
+//        return roadSegment.checkIfLaneOnLeft(laneSegmentId);
+//    }
+//
+//    public LaneSegment getLaneOnLeft(int laneId) {
+//        return roadSegment.getLaneOnLeft(laneId);
+//    }
+//
+//    public LaneSegment getLaneOnRight(int laneId) {
+//        return roadSegment.getLaneOnRight(laneId);
+//    }
+//
+//    /*Ne fonctionne que pour une ligne droite*/
+//    public boolean checkIfChangeLaneIsSafe(double position, LaneSegment lane) {
+//        if (lane.laneSegmentId != laneSegmentId) {
+//            return roadSegment.checkIfChangeLaneIsSafe(position, lane);
+//        }
+//        Vehicle vehicle = vehicles.getFirst();
+//        for (int i = 1; position > vehicle.getPosition() && i < vehicles.size(); i++) {
+//            if ((position - vehicle.getPosition()) < 5) {
+//                return false;
+//            }
+//            vehicle = vehicles.get(i);
+//        }
+//        if ((vehicle.getPosition() - position) < 5) {
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    /*Ajoute un véhicule en les classant par leur position sur la voie
+//     * le premier véhicule dans la liste est le plus proche du début de la ligne, le dernier le plus éloigné
+//     * !! Ne contrôle pas si le véhicule est déjà dans cette voie*/
+//    public void addVehicle(Vehicle vehicle) {
+//        int i;
+//        for (i = 0; i < vehicles.size(); i++) {
+//            if (vehicle.getPosition() > vehicles.get(i).getPosition()) {
+//                i++;
+//                break;
+//            }
+//        }
+//        i--;
+//        vehicles.add(i, vehicle);
+//        nbVehicle++;
+//    }
+//
+//    public void removeVehicle(Vehicle vehicle) {
+//        vehicles.remove(vehicle);
+//        nbVehicle --;
+//    }
+
 }
