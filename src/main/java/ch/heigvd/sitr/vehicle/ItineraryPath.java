3dd354a5a33c0287b550ad86171be35494039d42
Simon Walther <simon.walt@hotmail.com>
2019-04-11 10:03:29 +0200

    add throws clause to javadoc #71

--- src/main/java/ch/heigvd/sitr/vehicle/ItineraryPath.java
+++ src/main/java/ch/heigvd/sitr/vehicle/ItineraryPath.java
@@ -11,6 +11,7 @@ import java.awt.geom.Point2D;
 
 /**
  * ItineraryPath class represents the a stop in an vehicle's itinerary
+ *
  * @author Simon Walther
  */
 public class ItineraryPath {
@@ -24,19 +25,21 @@ public class ItineraryPath {
 
     /**
      * Constructor
-     * @param origin the point of origin
+     *
+     * @param origin      the point of origin
      * @param destination the point of destination
+     * @throws IllegalArgumentException if we specify a null destination or origin
      */
     public ItineraryPath(Point2D.Double origin, Point2D.Double destination) throws IllegalArgumentException {
         // ensure that origin is not empty
-        if(origin == null) {
+        if (origin == null) {
             throw new IllegalArgumentException("Itinerary stop must have an origin");
         }
 
         this.origin = origin;
 
         // ensure that destination is not empty
-        if(destination == null) {
+        if (destination == null) {
             throw new IllegalArgumentException("Itinerary stop must have a destination");
         }
 
