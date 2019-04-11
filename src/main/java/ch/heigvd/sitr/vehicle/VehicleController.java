eb518065b973656007bb271ea895a759c70ff1cf
Simon Walther <simon.walt@hotmail.com>
2019-04-11 11:08:27 +0200

    create jar with required dependencies, update jdom

--- src/main/java/ch/heigvd/sitr/vehicle/VehicleController.java
+++ src/main/java/ch/heigvd/sitr/vehicle/VehicleController.java
@@ -7,10 +7,10 @@ package ch.heigvd.sitr.vehicle;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 import java.io.*;
 
