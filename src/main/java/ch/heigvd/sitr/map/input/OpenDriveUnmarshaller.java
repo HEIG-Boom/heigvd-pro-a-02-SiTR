30e6b0082008e6bf2d1b974cc8898ef1267e012d
Simon Walther <simon.walt@hotmail.com>
2019-05-06 09:54:32 +0200

    fix jar packaging, getResource doesn't work inside jar so use getResourceAsStream instead

--- src/main/java/ch/heigvd/sitr/map/input/OpenDriveUnmarshaller.java
+++ src/main/java/ch/heigvd/sitr/map/input/OpenDriveUnmarshaller.java
@@ -9,7 +9,7 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
-import java.io.File;
+import java.io.*;
 
 /**
  * This class is used to unmarshall an XML Schema using JAXB. Unmarshall is the process of
@@ -27,24 +27,25 @@ public class OpenDriveUnmarshaller {
      * This method unmarshalls the xmlFile and generates an instance with values available in
      * XML document. Before that, we have bind the schema and generate the classes
      *
-     * @param xmlFile The XML file to unmarshall with JAXB
+     * @param xmlFile The XML file source to unmarshall with JAXB
      * @return Instance with values available in XML document.
      */
-    public OpenDRIVE load(File xmlFile) {
+    public OpenDRIVE load(StreamSource xmlFile) {
         OpenDRIVE data = null;
 
         try {
-            File xsdFile = new File(OpenDriveUnmarshaller.class.getResource(BASE_PATH + XSDFILE).getFile());
+            InputStream in = getClass().getResourceAsStream(BASE_PATH + XSDFILE);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
             // Build XML Schema based on openDRIVE xsd file
-            Schema schema = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI).newSchema(xsdFile);
+            Schema schema = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI).newSchema(new StreamSource(br));
 
             // Create the unmarshaller
             JAXBContext context = JAXBContext.newInstance(OD_CLASS);
             Unmarshaller unmarshaller = context.createUnmarshaller();
             unmarshaller.setSchema(schema);
 
-            StreamSource source = new StreamSource(xmlFile);
+            StreamSource source = xmlFile;
             data = (OpenDRIVE) unmarshaller.unmarshal(source, OD_CLASS).getValue();
         } catch (SAXException e) {
             e.printStackTrace();
