679c0454a1f2b1101cfbcad88d61b39b07dd054b
Laykel <layke.luc@gmail.com>
2019-05-09 20:04:46 +0200

    Added color to VehiclesControllerType config

--- src/main/java/ch/heigvd/sitr/model/VehicleControllerType.java
+++ src/main/java/ch/heigvd/sitr/model/VehicleControllerType.java
@@ -7,23 +7,28 @@ package ch.heigvd.sitr.model;
 
 import lombok.Getter;
 
+import java.awt.*;
+
 /**
  * Enum for controller types, provides controller names and paths to data files
  *
  * @author Luc Wachter
  */
 public enum VehicleControllerType {
-    TIMID("Timide", "timid.xml"),
-    CAREFUL("Prudent", "careful.xml"),
-    RECKLESS("Téméraire", "reckless.xml"),
+    TIMID("Timide", Color.GRAY, "timid.xml"),
+    CAREFUL("Prudent", Color.BLUE, "careful.xml"),
+    RECKLESS("Téméraire", Color.RED, "reckless.xml"),
     // Autonomous vehicles can alter the properties afterwards
-    AUTONOMOUS("Autonome", "autonomous.xml");
+    AUTONOMOUS("Autonome", Color.BLACK, "autonomous.xml");
 
     // Base folder containing config files for controllers
     private static final String BASE_PATH = "/vehicleController/";
 
     // Name of the controller (to display in GUI and such)
     private final String name;
+    // Default color for a vehicle using this controller
+    @Getter
+    private final Color color;
     // Path to the controller's data file
     @Getter
     private final String configPath;
@@ -34,8 +39,9 @@ public enum VehicleControllerType {
      * @param name       The name of the controller
      * @param configFile The name of the controller's data file (with extension)
      */
-    VehicleControllerType(String name, String configFile) {
+    VehicleControllerType(String name, Color color, String configFile) {
         this.name = name;
+        this.color = color;
         // Construct path to data file from base path and filename
         configPath = BASE_PATH + configFile;
     }
